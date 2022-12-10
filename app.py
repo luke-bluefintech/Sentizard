f2413ebdafc8ff64231b3b362fd4858ebfd13077
Luke Foley <luke@bluefintechpartners.com>
2022-12-10 16:08:42 -0500

    initial front end pushes

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__, template_folder=curr_dir)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("index_proto.html")
+    return render_template("index.html")
 
 
 @app.route('/result', methods=['POST', 'GET'])
@@ -26,16 +26,17 @@ def result():
 
     ##############################
     # Analyze the sentiment of the sentence
-    val = backend.analyze_sentiment(sentence, model_name)
-    val_str = str(val)
+    # val = backend.analyze_sentiment(sentence, model_name)
+    # val_str = str(val)
+    val_str = '100'
     ##############################
 
-    return render_template('index_proto.html', res=val_str)
+    return render_template('index.html', res=val_str)
 
 
 if __name__ == "__main__":
     # Load backend
-    backend = Backend()
+    # backend = Backend()
 
     # Launch website
     app.run(debug=True)
