c171a850015676cdd212afc47421ed2029f1d092
Luke Foley <luke@bluefintechpartners.com>
2022-12-12 21:40:30 -0500

    formatting changes and starting to implement BiLSTM

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request
+#from backend.scripts.nn_training import Bi_LSTM
 from backend.scripts.backend import Backend
 
 app = Flask(__name__)
@@ -24,6 +25,9 @@ def result():
         val = backend.analyze_sentiment(sentence, "C-LSTM")
     elif algorithm == "SVM":
         val = backend.analyze_sentiment(sentence, "SVM")
+    elif algorithm == "BiLSTM":
+        print("BiLSTM!")
+        #val = Bi_LSTM()
     sentence = str(val)
 
     return render_template('index.html', sentence=sentence, date1=date1, date2=date2, city=city, algorithm=algorithm)
